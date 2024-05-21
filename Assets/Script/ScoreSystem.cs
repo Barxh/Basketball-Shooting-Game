1d022d568ec3facc87da160180ffb1a4c20f2fff
jandricanja <155174114+jandricanja@users.noreply.github.com>
2024-05-21 14:23:58 +0200

    ballSpawn

--- Assets/Script/ScoreSystem.cs
+++ Assets/Script/ScoreSystem.cs
@@ -5,6 +5,12 @@ using TMPro;
 public class ScoreScript : MonoBehaviour
 {
     // Start is called before the first frame update
+    
+
+    [SerializeField]
+    private GameObject ballPrefab;
+    [SerializeField]
+    private SpawnArea spawnArea;
     [SerializeField]
     private TMP_Text scoreText;
     private int score = 0;
@@ -14,5 +20,9 @@ public class ScoreScript : MonoBehaviour
         scoreText.SetText(score.ToString());
     }
 
+    public void startGame(){
+        GameObject ball=Instantiate(ballPrefab);
+        spawnArea.spawnBall(ball.transform);
+    }
 
 }
